ea43d6200668276bea519b6290e2fffcdaaebded
allen <allenny@163.com>
2019-08-23 17:48:12 +0800

    refactor

--- src/main/java/com/github/swiftech/swiftmarker/ProcessContext.java
+++ src/main/java/com/github/swiftech/swiftmarker/ProcessContext.java
@@ -2,7 +2,6 @@ package com.github.swiftech.swiftmarker;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeMap;
